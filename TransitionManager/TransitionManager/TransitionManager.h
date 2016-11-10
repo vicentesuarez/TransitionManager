7cd3dba1fc4b84411595baee68f3ab1b075abdfc
Vicente Suarez <vicentesuarez88@yahoo.com>
2016-11-09 22:27:45 -0800

    Added Xcode project

---
+++ TransitionManager/TransitionManager/TransitionManager.h
@@ -0,0 +1,19 @@
+//
+//  TransitionManager.h
+//  TransitionManager
+//
+//  Created by Vicente Suarez on 11/9/16.
+//  Copyright © 2016 Vicente Suarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransitionManager.
+FOUNDATION_EXPORT double TransitionManagerVersionNumber;
+
+//! Project version string for TransitionManager.
+FOUNDATION_EXPORT const unsigned char TransitionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitionManager/PublicHeader.h>
+
+
