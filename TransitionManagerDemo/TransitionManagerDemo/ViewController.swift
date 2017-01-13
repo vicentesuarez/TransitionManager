84895f53a05447b6d271d0633aebb98542fbf3b2
Vicente Suarez <vicentesuarez88@yahoo.com>
2017-01-13 01:25:10 -0800

    TM-5
    Created demo using transition manager

--- TransitionManagerDemo/TransitionManagerDemo/ViewController.swift
+++ TransitionManagerDemo/TransitionManagerDemo/ViewController.swift
@@ -9,18 +9,21 @@
 import UIKit
 import TransitionManager
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TransitionSource {
+    
+    var transitionManager: TransitionManager!
 
+    @IBAction func unwindToMain(_ segue: UIStoryboardSegue) {}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        transitionManager = TransitionManager(source: self, presentationType: PresentationType.segue(presenting: "PresentMenuSegue", dismissing: "DismissMenuSegue"))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let menuViewController = segue.destination as? MenuViewController {
+            transitionManager.setDestination(menuViewController)
+        }
     }
-
-
 }
 
