4861a2807e2afe90ac5d2adbbc8baff826c4a180
Vicente Suarez <vicentesuarez88@yahoo.com>
2017-01-13 00:37:24 -0800

    TM-3 TM-4
    Created Transition Manager Demo project and added TransitionManager framework built with Carthage

---
+++ TransitionManagerDemo/TransitionManagerDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TransitionManagerDemo
+//
+//  Created by Vicente Suarez on 1/13/17.
+//  Copyright © 2017 Vicente Suarez. All rights reserved.
+//
+
+import UIKit
+import TransitionManager
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
